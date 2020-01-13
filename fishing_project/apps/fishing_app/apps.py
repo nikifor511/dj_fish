4d890131bf413aa4e185dc787cf1ee2dc6311b9b
nikifor511 <deadnikifor@gmail.com>
2020-01-13 17:06:09 +0300

    c

---
+++ fishing_project/apps/fishing_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FishingAppConfig(AppConfig):
+    name = 'fishing_app'
---
+++ fishing_project/apps/fishing_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class FishingPlace(models.Model):
+     name = models.CharField('Name of place', max_length=100)
+     lant = models.DecimalField(max_digits=10, decimal_places=8)
+     long = models.DecimalField(max_digits=10, decimal_places=8)
+     description = models.TextField('Description of place')
+     photos = models.TextField('Photos of place')
+
+class PlaceOrder(models.Model):
+    FishingPlace
+
